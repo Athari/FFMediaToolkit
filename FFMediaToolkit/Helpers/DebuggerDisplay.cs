ff54acd20a7283ecc5881161df86c704993d266d
Athari <athari.p@gmail.com>
2025-11-03 01:05:04 +0300

    Added DebuggerDisplay attributes for AVCodec, AVDeviceRect, AVRational.

---
+++ FFMediaToolkit/Helpers/DebuggerDisplay.cs
@@ -0,0 +1,6 @@
+﻿using System.Diagnostics;
+using FFmpeg.AutoGen;
+
+[assembly: DebuggerDisplay("AVCodec id={id}", Target = typeof(AVCodec))]
+[assembly: DebuggerDisplay("AVDeviceRect {x} {y} {width} {height}", Target = typeof(AVDeviceRect))]
+[assembly: DebuggerDisplay("AVRational {num}/{den}", Target = typeof(AVRational))]
